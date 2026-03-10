cc7553a84fb42d408645a7200dc7a5d6a15e3d77
Miek Gieben <miek@miek.nl>
2026-03-10 16:16:29 +0100

    Play with fmt
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/mclfmt/mclfmt.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/purpleidea/mgmt/lang/interfaces"
+	"github.com/purpleidea/mgmt/lang/parser"
+)
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	xast, err := parser.LexParse(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	xast.Apply(func(node interfaces.Node) error {
+		fmt.Printf("node %+v\n", node)
+		return nil
+	})
+
+	fmt.Printf("behold, the AST: %+v", xast)
+}
